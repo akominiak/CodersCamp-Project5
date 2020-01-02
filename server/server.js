788e4fb45b29cfff0644a43fbabbe2fc6240cc72
Żaneta <zanbor3@gmail.com>
2020-01-02 17:31:44 +0100

    Added database configuration

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+
+mongoose.connect('mongodb://localhost/todoList')
+    .then(() => console.log('Connected to todo List Base'))
+    .catch(err => console.error('Could not connect to todo List Base', err));
+
+
+    const port = process.env.PORT || 8080;
+    app.listen(port, () => console.log(`Listening on port ${port}...`));
